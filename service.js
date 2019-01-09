4601c802448baf260f71b82d998d3eeb362ac4a9
Syahrul <pauscrypto@gmail.com>
2019-01-09 15:21:19 +0800

    Modified for use in obsidian

--- service.js
+++ service.js
@@ -4,9 +4,39 @@ const TurtleService = require('./')
 const util = require('util')
 
 var wallet = new TurtleService({
-  containerFile: './container.walletd',
-  rpcPassword: 'changeme'
+  appName: 'default', // This defines an application name used to store some settings.
+  pollingInterval: 10, // Check to make sure that turtle-service is alive every x seconds
+  maxPollingFailures: 3, // After the polling checks fail x times, report the turtle-service process down
+  saveInterval: 10, // issue an automatic save request every x seconds as long as the wallet is synced
+  scanInterval: 5, // scan the wallet for new transactions every x seconds as long as the wallet is synced
+  timeout: 2000, // consider RPC calls timed out after x milliseconds
+  path: './turtle-service', // the path to the turtle-service binary
+  enableWebSocket: true, // enable the WebSocket server at bindPort + 1
+
+  // Standard turtle-service options start here
+  config: false, // the path to a turtle-service config file -- if you so choose
+  bindAddress: '127.0.0.1', // The IP address that turtle-service will bind to
+  bindPort: 8070, // The port that turtle-service will bind to
+  rpcPassword: 'x9098bc', // You really should use an RPC password
+  rpcLegacySecurity: false, // Turning this to true, removes the requirement for a RPC password, either rpcPassword or rpcLegacySecurity MUST be set
+  containerFile: './container.walletd', // The path to your turtle-service container file
+  containerPassword: 'password', // The password to your turtle-service container file
+  logFile: false, // The path to the log file you would like turtle-service to keep
+  logLevel: 4, // The log level to use with turtle-service
+  syncFromZero: false, // If set to true, will tell turtle-service to always sync the container from zero.
+  daemonRpcAddress: '127.0.0.1', // Daemon RPC IP Address (if your daemon doesn't use 127.0.0.1 or 0.0.0.0 -- you really need to change this)
+  daemonRpcPort: 11898, // Daemon RPC port
+
+  // RPC API default values
+  defaultMixin: 3, // the default mixin to use for transactions
+  defaultFee: 0.1, // the default transaction fee for transactions
+  defaultBlockCount: 1, // the default number of blocks when blockCount is required
+  decimalDivisor: 100000000, // Currency has many decimal places?
+  defaultFirstBlockIndex: 1, // the default first block index we will use when it is required
+  defaultUnlockTime: 0, // the default unlockTime for transactions
+  defaultFusionThreshold: 10000000, // the default fusionThreshold for fusion transactions
 })
+
 
 function log (message) {
   console.log(util.format('%s: %s', (new Date()).toUTCString(), message))
